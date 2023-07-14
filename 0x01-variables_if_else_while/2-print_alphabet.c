c058a59ea4b1ed7c85676b9f075091ea097f3a6b
thevorgx <kh.elallali@gmail.com>
2023-07-14 16:33:56 -0700

    betty again

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,18 @@
-/* main C program to print the alphabet in lowercase */
+// main C program to print the alphabet in lowercase
 
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    /* Get the character to be written */
-	
-char alpha;
+    // Get the character to be written
+    char ch;
 
-/* Write the characters from 'a' to 'z' */
-for (alpha = 'a'; alpha <= 'z'; alpha++)
-putchar(alpha\n);
-return 0;
+    // Write the characters from 'a' to 'z'
+    for (ch = 'a'; ch <= 'z'; ch++)
+        putchar(ch);
+
+    // Print a newline character
+    putchar('\n');
+
+    return 0;
 }
-
