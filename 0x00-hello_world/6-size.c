0944937f74d3c0d7487bca2f4b237aeca02f9536
thevorgx <kh.elallali@gmail.com>
2023-07-13 17:35:16 -0700

    Size matters even in C!

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+printf("%c\n",sizeof(char));
+printf("%d\n",sizeof(int));
+printf("%ld\n",sizeof(long int));
+printf("%lld\n",sizeof(long long int));
+printf("%f\n",sizeof(float));
+return(0);
+}
