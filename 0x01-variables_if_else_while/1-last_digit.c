3706301b3f58fb27541bf1dbd236dcce40a480ba
thevorgx <kh.elallali@gmail.com>
2023-07-14 14:48:01 -0700

    first digit of the number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - generate random int number, is it posetive, negative, or equal to 0 ?
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n;
+        int v;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        v = n % 10;
+        if (v > 5)
+        {
+                printf("Last digit of %d is %d and is greater than 5\n", n,v);
+        }
+        else if (v < 6)
+        {
+                printf("Last digit of %d is %d and is less than 6\n", n,v);
+        }
+        else if (v == 0)
+        {
+                printf("Last digit of %d is %d and is 0\n", n,v);
+        }
+
+        return (0);
+}
