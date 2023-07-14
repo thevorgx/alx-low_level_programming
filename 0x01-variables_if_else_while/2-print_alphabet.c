b8d1fd71cc1541ede716ada7905eaec9b06821bb
thevorgx <kh.elallali@gmail.com>
2023-07-14 16:36:55 -0700

    ?

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
-// main C program to print the alphabet in lowercase
-
 #include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 
 int main()
 {
-    // Get the character to be written
-    char ch;
+   char ch;
 
-    // Write the characters from 'a' to 'z'
     for (ch = 'a'; ch <= 'z'; ch++)
         putchar(ch);
 
-    // Print a newline character
     putchar('\n');
 
     return 0;
