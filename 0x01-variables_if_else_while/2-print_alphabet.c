1b8991077926d7e3d2636fe4b2d7c673c6a836a6
thevorgx <kh.elallali@gmail.com>
2023-07-14 16:10:02 -0700

    write lowercase alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+/* C program to print the alphabet in lowercase */
+
+#include <stdio.h>
+
+int main(void)
+{
+    /* Get the character to be written */
+    char alpha;
+
+    /* Write the characters from 'a' to 'z' */
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
+        putchar(alpha);
+
+    return 0;
+}
+
