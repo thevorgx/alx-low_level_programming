736f98247fdd34ad0efbadd765457f43bb4ecfab
thevorgx <kh.elallali@gmail.com>
2023-07-14 17:46:57 -0700

    same but no e no q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+if (alpha == 'e' || alpha == 'q')
+continue; // Skip 'e' and 'q'
+
+putchar(alpha);
+}
+
+putchar('\n');
+
+    return (0);
+}
+
