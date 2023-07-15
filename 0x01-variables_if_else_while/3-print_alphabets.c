b4750fc799c6d5caa935e2c8b214b32f04daaf83
thevorgx <kh.elallali@gmail.com>
2023-07-14 17:09:25 -0700

    try 1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+	putchar(alpha);
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+        putchar(alpha);
+
+putchar('\n');
+
+return (0);
+}
