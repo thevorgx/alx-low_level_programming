30468145c6fcee0b9eb125429eba16cba3a963f2
thevorgx <kh.elallali@gmail.com>
2023-07-14 18:41:29 -0700

    print from 0 to 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,22 +7,22 @@
  */
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
 int i;
-    
-for (i = 0; i <= 9; i++) 
+
+for (i = 0; i <= 9; i++)
 {
 putchar('0' + i);
-        
-if (i != 9) 
+
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
-    
+
 putchar('\n');
-    
+
 return (0);
 }
