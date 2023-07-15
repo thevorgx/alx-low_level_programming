6019661023537b77888389700366581049b598bb
thevorgx <kh.elallali@gmail.com>
2023-07-14 18:21:48 -0700

    print alpha in rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alpha;
+
+for (alpha = 'z'; alpha >= 'a'; alpha--)
+putchar(alpha);
+
+putchar('\n');
+
+return (0);
+}
