4d75e7b2141b71c56e7abba3103995f8b1e91144
thevorgx <kh.elallali@gmail.com>
2023-07-14 18:31:00 -0700

    print hexa using putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point Print hexa with putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 15; i++)
+{
+if (i < 10)
+putchar(i + '0');
+else
+putchar(i - 10 + 'a');
+}
+putchar('\n');
+
+return (0);
+}
