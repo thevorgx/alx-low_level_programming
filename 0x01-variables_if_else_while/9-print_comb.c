08929cd346114e30c89031a3c855477d0815c2a5
thevorgx <kh.elallali@gmail.com>
2023-07-14 18:38:08 -0700

    print number from 0 to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point Print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void) 
+{
+int i;
+    
+for (i = 0; i <= 9; i++) 
+{
+putchar('0' + i);
+        
+if (i != 9) 
+{
+putchar(',');
+putchar(' ');
+}
+}
+    
+putchar('\n');
+    
+return (0);
+}
