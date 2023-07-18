ae67557a661a5df0bdb777eff3b396768a80dc5c
thevorgx <kh.elallali@gmail.com>
2023-07-18 16:12:34 -0700

    alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+	for (i=0 ; i=9 ;i++)
+	{
+		for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+
+		_putchar(alpha);
+	_putchar ('\n');
+	}
+}
