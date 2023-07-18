c31aa0d46a036ba9e14857dd68036872b114bee7
thevorgx <kh.elallali@gmail.com>
2023-07-18 16:18:58 -0700

    alpha x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code
+ * print_alphabet_x10 - check the code
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
+
 	char alpha;
+
 	int i;
-	for (i=0 ; i<=10 ;i++)
+
+	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (alpha = 'a' ; alpha <= 'z' ; alpha++)
 
