e39b18d84b18369c664b1d8db9c73d3b9900662d
thevorgx <kh.elallali@gmail.com>
2023-07-18 15:52:16 -0700

    write alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+    char alpha = 'a';
+    for (alpha = 'a' ; alpha = 'z' ; alpha++)
+            _putchar(alpha);
+    _putchar ('\n');
+
+}
