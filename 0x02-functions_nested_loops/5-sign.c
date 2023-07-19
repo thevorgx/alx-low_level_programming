f6c1644e4861ee3726a325ee66dd2d9fc78b8cce
thevorgx <kh.elallali@gmail.com>
2023-07-18 17:54:41 -0700

    if test

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * @n: the char to check
+ * int print_sign(int n) - take the output of this function and see
+ * prints the sign of a number
+ * Return: Always 0.
+ */
+int print_sign(int n);
+{
+	if (n > +)
+		return (1);
+		_putchar(write[43]);
+	else if (c = 0 )
+		_putchar(write[48]);
+		return (1);
+	else
+		_putchar(write[45]);
+                return (-1);
+}
