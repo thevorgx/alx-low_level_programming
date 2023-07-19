17b4f53d77b64339a1a21e1e59001d6b559809ff
thevorgx <kh.elallali@gmail.com>
2023-07-18 17:00:11 -0700

    check lower and upper case

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islower -> check for lower case char
+ * isalpha -> check for lower case char
  * @c: the char to check
- * int _islower(int c) - take the output of this function and see
+ * int _isalpha - take the output of this function and see
  * if its lowercase using ASCII
  * Return: Always 0.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
