2ad861ac3ea9522bc80b5e564d5de2e6a48200c6
thevorgx <kh.elallali@gmail.com>
2023-07-18 19:55:17 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Adds n1 and n2
+ *
+ * @n1: Parameter
+ * @n2: Parameter
+ *
+ * Return: n1+n2
+ */
+
+int add(int n1, int n2)
+{
+	int add;
+
+	add = n1 + n2;
+	return (add);
+}
