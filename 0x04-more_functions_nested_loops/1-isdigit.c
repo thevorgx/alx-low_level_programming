757b3b4d828bbba0272a96c6709a79c34d38f680
thevorgx <kh.elallali@gmail.com>
2023-07-20 16:05:54 -0700

    check for digits between 0 and 9 and both numbers included (using ASCII)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _isdigit -checks fo digits
+ * @c: the char from the proto to verify
+ * Return: 1 for digits and 0 for else
+ */
+
+
 int _isdigit(int c)
 {
 	if (c >= '0' || c <= '9')
