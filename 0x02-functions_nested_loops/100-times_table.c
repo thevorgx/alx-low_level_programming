c5496f2b170764dacc8340399a923788f1fdf16a
thevorgx <kh.elallali@gmail.com>
2023-07-19 19:34:05 -0700

    first test

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table -  prints n tables, initial 0
+ * @n: input function
+ */
+
+void print_times_table(int n)
+{
+	 n1, n2;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (n1 = 0; n1 <= n; n++)
+		{
+			_putchar('0');
+			for (n2 = 1; n2 <= n; n2++)
+			{
+				_putchar(',');
+				if (n1 * n2 <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(n1 * n2 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					if (n1 * n2 <= 99)
+					{
+						_putchar(' ');
+						_putchar((n1 * n2) / 10 + '0');
+					}
+					else
+					{
+						_putchar((n1 * n2) / 100 + '0');
+						_putchar(((n1 * n2) / 10) % 10  + '0');
+
+					}
+					_putchar((n1 * n2) % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
