0609bb128593e4616f90d3a4a7063dca941f289f
thevorgx <kh.elallali@gmail.com>
2023-07-20 19:01:33 -0700

    largest prime factor test 1

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - The prime factors
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int v = 612852475143;
+	long int o = 2
+	long int fac=o;
+
+	d = 2;
+	n = 612852475143;
+	while (v != 1)
+	{
+		if (v % o == 0)
+		{
+			v /= o;
+			fac = o;
+		}
+		o++;
+	}
+	printf("%ld\n", fac);
+return (0);
+}
