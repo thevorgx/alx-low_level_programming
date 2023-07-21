817db8a79c6f1e2fa71116b307ea9ea476b3c6fc
thevorgx <kh.elallali@gmail.com>
2023-07-20 18:39:18 -0700

    illuminaty stuff

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - draw a triangle
+ * @size: triangle size
+ *
+ * Return: nothing, nada, void
+ */
+void print_triangle(int size)
+{
+	int i, j, v;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return
+	}
+
+	for (i = 0; i < size; ++i)
+	{
+		for (j = size - i; j > 1; --j)
+			_putchar(' ');
+		for (v = 0; v <= i; ++v)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
