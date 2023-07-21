adfc82020c58ef9793149fa62c879a323d73b2f1
thevorgx <kh.elallali@gmail.com>
2023-07-20 17:07:22 -0700

    print numbers from 0 to 9 skip 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print number from 0 to 9 but skip 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int v;
+
+	for (v = '0' ; v <= '9' ; v++)
+	{
+		if (v != '2' && v != '4')
+			_putchar(v);
+		_putchar('\n');
