bd5de8fcbb7a163bada8ecd898f825df65750c61
thevorgx <kh.elallali@gmail.com>
2023-07-20 18:09:20 -0700

    printing a line if n>0 if not just return to new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - straight line draw
+ *
+ * @n: int para
+ */
+
+void print_line(int n)
+{
+	int v;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (v = 1; i <= n; v++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
