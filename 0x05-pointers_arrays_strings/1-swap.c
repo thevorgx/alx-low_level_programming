c1814b24e8f469ec77932fba8d7449c2cb1ac65b
thevorgx <kh.elallali@gmail.com>
2023-07-24 16:18:33 -0700

    swaping the value of int a and int b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * reset_to_98 - check the code
+ *@n: the parameter to point to 98
+ * Return: bo return
+ */
+void swap_int(int *a, int *b)
+{
+	*temp = a;
+	a = b;
+	b = temp;
