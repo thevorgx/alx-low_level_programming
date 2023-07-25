6be471f0077a486bba77459c0d2c39a9f7bc9518
thevorgx <kh.elallali@gmail.com>
2023-07-24 17:16:55 -0700

    find length betty update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include"main.h"
 
 /**
- * swap_int - check the code
- *@*s: pointer *s that we increment to be able to increment the value of len
+ *_strlen - check the code
+ *@s: pointer *s that we increment to be able to increment the value of len
  *@len: the value we want to return so we know the length of the string
  * Return: len
  */
@@ -10,7 +10,7 @@ int _strlen(char *s)
 {
 	int len = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		++len;
 		++s;
