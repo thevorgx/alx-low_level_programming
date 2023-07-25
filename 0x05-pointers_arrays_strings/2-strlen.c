04087e419d10b177d8210672fcac4c6a9624ef32
thevorgx <kh.elallali@gmail.com>
2023-07-24 17:00:52 -0700

    know the length func

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * swap_int - check the code
+ *@*s: pointer *s that we increment to be able to increment the value of len
+ *@len: the value we want to return so we know the length of the string
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(*s != '\0')
+	{
+		++len;
+		++*s;
+	}
+	return (len);
+}
