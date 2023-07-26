e20643e0572c56d968d61abf93e7a3def2525209
thevorgx <kh.elallali@gmail.com>
2023-07-25 17:23:09 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string -> reverse string func
+ *
+ * @s: Param
+ */
+
+void rev_string(char *s)
+{
+	int i , swp , j , v
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	j = i - 1;
+	for (v = 0; j >= 0 && v < j; j--, v++)
+	{
+		swp = s[v];
+		s[v] = s[j];
+		s[j] = swp;
+	}
+}
