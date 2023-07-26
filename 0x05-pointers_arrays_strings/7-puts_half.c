8c1704696b02f280642b9d5723517e77000100e6
thevorgx <kh.elallali@gmail.com>
2023-07-25 17:54:01 -0700

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half -> Prints last half of a string
+ *
+ * @str: Input
+ */
+
+void puts_half(char *str)
+{
+	int len, v;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		continue;
+	}
+	if (len % 2 == 0)
+	{
+		v = len / 2;
+	}
+	else
+	{
+		v = (len - 1) / 2;
+		v++;
+	}
+	while (str[v] != '\0')
+	{
+		_putchar(str[v]);
+		v++;
+	}
+	_putchar('\n');
+}
