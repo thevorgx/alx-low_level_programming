d6290f04b168576eaa294aa5f9cd91a904dafe68
thevorgx <kh.elallali@gmail.com>
2023-07-25 18:24:01 -0700

    andom valid password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random password
+ *
+ * Return: 0, nothing, nada, walo
+ */
+
+int main(void)
+{
+	int summ;
+	char ran;
+
+	srand(time(NULL));
+	while (summ <= 2772)
+	{
+		ran = rand() % 128;
+		summ += ran;
+		putchar(ran);
+	}
+	putchar(2772 - summ);
+	return (0);
+}
