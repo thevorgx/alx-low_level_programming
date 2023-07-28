7e3efda506417c194e085329922b5f9693240e2d
thevorgx <kh.elallali@gmail.com>
2023-07-27 19:17:28 -0700

    reversing array elements

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reversing array element
+ * @a: array para
+ * @n: n elems for the array para
+ */
+void reverse_array(int *a, int n)
+{
+    int i = 0;
+    int rvs;
+
+    while (i < n)
+    {
+        rvs = a[i];
+        a[i] = a[n - 1];
+        a[n - 1] = rvs;
+        i++;
+        n--
+    }
+}
