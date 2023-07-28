a9dc228ea58d38b5d1857c22bbacaf8bab9b8ab1
thevorgx <kh.elallali@gmail.com>
2023-07-27 18:54:43 -0700

    4th try

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; ++i)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	for (i = 0; i < n; ++i)
+	for (; i < n; i++)
 	{
 		dest[i] = '\0';
 	}
