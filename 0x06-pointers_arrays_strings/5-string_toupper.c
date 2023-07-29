9756c94d27689975f5a8c1296f25a2a08e51d9d7
thevorgx <kh.elallali@gmail.com>
2023-07-28 18:08:15 -0700

    lower case to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,18 +4,18 @@
  * *string_toupper - change minis letters to maj
  *
  * @str: func param
- * 
+ *
  * Return: str
  */
 
 char *string_toupper(char *str)
 {
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-            str[i] -= 32;
-        i++;
-    }
-    return (str);
+	int i = 0;
+	while (str[i] != '\0')
+	{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	str[i] -= 32;
+	i++;
+	}
+	return (str);
 }
