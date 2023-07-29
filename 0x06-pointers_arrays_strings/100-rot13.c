ed0e81aa1f92f5965f1f334786a754aad17b1401
thevorgx <kh.elallali@gmail.com>
2023-07-28 18:48:38 -0700

    encoding using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include"main.h"
+
+/**
+ * *rot13 - encoding using Rot13
+ *
+ * @str: param str
+ *
+ * Return: str
+ */
+
+char *rot13(char *str)
+{
+    int i = 0;
+    int j = 0;
+    char *p1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char *p2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+    while (str[i] != '\0')
+    {
+        j = 0;
+        while (p1[j] != '\0')
+        {
+            if (str[i] == p1[j])
+            {
+                str[i] = p2[j];
+                break;
+            }
+            j++;
+        }
+
+        i++;
+    }
+
+    return (str);
+}
