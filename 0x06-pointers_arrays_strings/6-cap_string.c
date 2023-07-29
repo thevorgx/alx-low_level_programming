00053653d95171ab878a8367290188baaadfa7e7
thevorgx <kh.elallali@gmail.com>
2023-07-28 18:17:37 -0700

    caipitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * *cap_string -  function that capitalizes all words of a string
+ *
+ * @str: str param
+ *
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+    int i = 0;
+    int j = 0;
+    char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+    while (str[i] != '\0')
+    {
+        if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+            str[i] -= 32;
+
+        for (j = 0; j < 13; j++)
+        {
+            if (str[i - 1] == sep[j - 1])
+            {
+                if (str[i] >= 'a' && str[i] <= 'z')
+                {
+                    str[i] -= 32;
+                }
+            }
+        }
+
+        i++;
+    }
+
+    return (str);
+}
+
+
