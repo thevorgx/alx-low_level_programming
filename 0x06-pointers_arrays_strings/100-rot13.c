28a2c3ac94bd9c56bac9ce2941ece9e92ac279dc
thevorgx <kh.elallali@gmail.com>
2023-07-28 18:49:25 -0700

    rot13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,26 +10,26 @@
 
 char *rot13(char *str)
 {
-    int i = 0;
-    int j = 0;
-    char *p1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char *p2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j = 0;
+	char *p1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *p2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    while (str[i] != '\0')
-    {
-        j = 0;
-        while (p1[j] != '\0')
-        {
-            if (str[i] == p1[j])
-            {
-                str[i] = p2[j];
-                break;
-            }
-            j++;
-        }
+	while (str[i] != '\0')
+	{
+	j = 0;
+	while (p1[j] != '\0')
+	{
+	if (str[i] == p1[j])
+	{
+	str[i] = p2[j];
+	break;
+	}
+	j++;
+	}
 
-        i++;
-    }
+	i++;
+	}
 
-    return (str);
+	return (str);
 }
