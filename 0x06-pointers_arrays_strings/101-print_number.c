842a158cad45be870480e946de7623717aec3634
thevorgx <kh.elallali@gmail.com>
2023-07-28 19:17:29 -0700

    updt8

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,23 +7,17 @@
 
 void print_number(int n)
 {
-	if (n == -2147483648)
-	{
-		_putchar('-');
-		_putchar('2');
-		print_number(147483648);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-		print_number(n);
-	}
-	else if (n > 9)
-	{
-		print_number(n / 10);
-		print_number(n % 10);
-	}
-	else
-		_putchar(n + '0');
+        unsigned int n1;
+
+        n1 = n;
+        if (n < 0)
+        {
+                _putchar('-');
+                n1 = -n;
+        }
+        if (n1 / 10 != 0)
+        {
+                print_number(n1 / 10);
+        }
+        _putchar(n1 % 10 + 48);
 }
