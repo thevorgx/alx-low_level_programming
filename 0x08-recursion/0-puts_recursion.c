c74183c49a2fd509ead234ecd6393cac503ef002
thevorgx <kh.elallali@gmail.com>
2023-08-02 14:18:47 -0700

    tilt update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,20 @@
-#include "main.h"
+#include"main.h"
+
+/**
+ * _puts_recursion - prints string
+ *
+ * @s: string param
+ * Return: Nothing, walo, nada, 0
+ */
+
 void _puts_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                _putchar('\n');
+	if (*s == '\0')
+	{
+	_putchar('\n');
 		return;
-        }
-        _putchar(*s);
-        _puts_recursion(s + 1);
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
