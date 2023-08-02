feeca71d24da6cc73c59d6e0c8f911754fd49e6d
thevorgx <kh.elallali@gmail.com>
2023-08-02 16:35:02 -0700

    checker issue

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - return the fact of a number
+ *
+ * @n: number.
+ * Return: number or fact
+ */
+int factorial(int n)
+{
+        if (n < 0)
+                return (-1);
+        if (n == 0)
+                return (1);
+        return (n * factorial(n - 1));
+}
