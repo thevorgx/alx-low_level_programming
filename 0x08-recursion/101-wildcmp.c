d0b3681528e08c4471846c78658a26e0eaa41900
thevorgx <kh.elallali@gmail.com>
2023-08-02 19:51:04 -0700

    update 4

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings with wildcards.
- * @s1: first string.
- * @s2: second string. It can contain '*' as a special character.
- * Return: 1 if the strings match, 0 otherwise.
+ * wildcmp - compares 2 str
+ * @s1: first string param
+ * @s2: second string. It can have '*' as a special char
+ * Return: 1 if the str is same, 0 otherwise.
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -16,12 +16,12 @@ int wildcmp(char *s1, char *s2)
 		}
 		return (*s2 == '\0');
 	}
-	
+
 	if (*s2 == '*')
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
-	
+
 	if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
