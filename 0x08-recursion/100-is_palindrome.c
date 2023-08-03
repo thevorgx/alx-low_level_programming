16ef9b3f2f48db8dae5dcefa3be87a76c5d61235
thevorgx <kh.elallali@gmail.com>
2023-08-02 19:40:38 -0700

    update 11

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,19 +16,17 @@ int _strlen_recursion(char *s)
 /**
  * vordet - detects if a string is a palindrome.
  * @s: string.
- * @n1: smallest iterator.
- * @n2: biggest iterator.
+ * @i: difference.
+ * @n: length of the string.
  * Return: 1 if s is a palindrome, 0 if not.
  */
-int vordet(char *s, int n1, int n2)
+int vordet(char *s, int i, int n)
 {
-    if (*(s + n1) == *(s + n2))
-    {
-        if (n1 == n2 || n1 == n2 + 1)
-            return (1);
-        return (0 + vordet(s, n1 + 1, n2 - 1));
-    }
-    return (0);
+    if (i >= n)
+        return (1);
+    if (*(s + i) != *(s + n))
+        return (0);
+    return (vordet(s, i + 1, n - 1));
 }
 
 /**
@@ -38,7 +36,9 @@ int vordet(char *s, int n1, int n2)
  */
 int is_palindrome(char *s)
 {
-    if (*s == '\0')
+    int len = _strlen_recursion(s);
+    if (len <= 1)
         return (1);
-    return (vordet(s, 0, _strlen_recursion(s) - 1));
+    return (vordet(s, 0, len - 1));
 }
+
