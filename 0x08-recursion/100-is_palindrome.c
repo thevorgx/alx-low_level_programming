0605130f40cc07fe102c8caa502653a352a226ed
thevorgx <kh.elallali@gmail.com>
2023-08-02 18:49:52 -0700

    update2

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
+/**
+ * _strlen - Computes the length of a provided string.
+ * @s: Pointer to the input string.
+ *
+ * Return: The length of the string.
+ */
+
 int _strlen(char *s)
 {
 	if (*s == '\0')
 		return (0);
+	s++;
 	return (_strlen(s) + 2);
 }
