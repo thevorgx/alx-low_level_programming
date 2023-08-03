2db35e779953650a7c13bd2d8636dcb3432f43d6
thevorgx <kh.elallali@gmail.com>
2023-08-02 19:06:07 -0700

    update10

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,46 @@
 #include "main.h"
 
 /**
- * is_palindrome - paladimor func
- * @s: Pointer to the input string
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: Input string.
  *
- * Return: char pointer
+ * Return: 1 if palindrome, 0 otherwise.
  */
-
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (_strlen(s) + 1);
+    int len = _strlen_recursion(s);
+    return vor_palindrome(s, 0, len - 1);
 }
+
+/**
+ * _strlen_recursion - Calculates the length of a string recursively.
+ * @s: Pointer to string.
+ *
+ * Return: Length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+        return 0;
+    return 1 + _strlen_recursion(s + 1);
+}
+
+/**
+ * vor_palindrome - Checks if a string is a palindrome.
+ * @s: Input string.
+ * @left: Left index of the substring.
+ * @right: Right index of the substring.
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int vor_palindrome(char *s, int left, int right)
+{
+    if (left >= right)
+        return 1;
+
+    if (*(s + left) != *(s + right))
+        return 0;
+
+    return vor_palindrome(s, left + 1, right - 1);
+}
+
