13d13f253b77e86760d3f7df2a35862290229610
thevorgx <kh.elallali@gmail.com>
2023-08-04 19:11:58 -0700

    update 10 betty again

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,28 +11,34 @@
  *
  * Return: (0) if successful, (1) if an error occurred.
  */
-int main(int argc, char *argv[]) {
-    int j = 1;
-    int summ = 0;
+int main(int argc, char *argv[])
+{
+	int j = 1;
+	int summ = 0;
 
-    if (argc == 1) {
-        printf("0\n");
-        return (0);
-    }
+	if (argc == 1)
+	{
+	printf("0\n");
+	return (0);
+	}
 
-    while (j <= argc - 1) {
-        int i = 0;
-        while (argv[j][i] != '\0') {
-            if (!isdigit(argv[j][i])) {
-                printf("Error\n");
-                return (1);
-            }
-            i++;
-        }
-        summ += atoi(argv[j]);
-        j++;
-    }
+	while (j <= argc - 1)
+	{
+		int i = 0;
 
-    printf("%d\n", summ);
-    return (0);
+	while (argv[j][i] != '\0')
+	{
+	if (!isdigit(argv[j][i]))
+	{
+	printf("Error\n");
+	return (1);
+	}
+	i++;
+	}
+	summ += atoi(argv[j]);
+	j++;
+	}
+
+	printf("%d\n", summ);
+	return (0);
 }
