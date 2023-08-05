69b43e4c03f77de976e9eae4e6e74e7e8b27e675
thevorgx <kh.elallali@gmail.com>
2023-08-04 17:56:56 -0700

    update 4

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,49 +10,49 @@
  */
 int main(int argc, char *argv[])
 {
-    int change = 0;
-    int num;
+	int change = 0;
+	int num;
 
-    /* Check if there is one command-line argument */
-    if (argc == 2)
-    {
-        num = atoi(argv[1]);
-        if (num > 0)
-        {
-            /* Calculate change */
-            while (num >= 25)
-            {
-                num -= 25;
-                change++;
-            }
-            while (num >= 10)
-            {
-                num -= 10;
-                change++;
-            }
-            while (num >= 5)
-            {
-                num -= 5;
-                change++;
-            }
-            while (num >= 2)
-            {
-                num -= 2;
-                change++;
-            }
-            while (num >= 1)
-            {
-                num -= 1;
-                change++;
-            }
-        }
+	/* Check if there is one command-line argument */
+	if (argc == 2)
+	{
+	num = atoi(argv[1]);
+	if (num > 0)
+	{
+	/* Calculate change */
+	while (num >= 25)
+	{
+	num -= 25;
+	change++;
+	}
+	while (num >= 10)
+	{
+	num -= 10;
+	change++;
+	}
+	while (num >= 5)
+	{
+	num -= 5;
+	change++;
+	}
+	while (num >= 2)
+	{
+	num -= 2;
+	change++;
+	}
+	while (num >= 1)
+	{
+	num -= 1;
+	change++;
+	}
+	}
 
-        printf("%d\n", change);
-        return (0);
-    }
+	printf("%d\n", change);
+	return (0);
+	}
 
-    
-    printf("Error\n");
-    return (1);
+
+	printf("Error\n");
+	return (1);
 }
 
