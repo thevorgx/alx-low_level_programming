0f9266b1e24381dd89c5b32253b8eabfc2b5fd50
thevorgx <kh.elallali@gmail.com>
2023-08-08 18:44:54 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,33 +11,33 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int total_length = 0;
-    int i;
-    int index = 0;
-    int j;
-    char *result;
+	int total_length = 0;
+	int i;
+	int index = 0;
+	int j;
+	char *result;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+	return (NULL);
 
-    result = (char *)malloc(total_length);
+	result = (char *)malloc(total_length);
 
-    for (i = 0; i < ac; i++)
-        total_length += strlen(av[i]) + 1;
+	for (i = 0; i < ac; i++)
+	total_length += strlen(av[i]) + 1;
 
-    result = (char *)malloc(total_length + 1);
-    if (result == NULL)
-        return (NULL);
+	result = (char *)malloc(total_length + 1);
+	if (result == NULL)
+	return (NULL);
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            result[index++] = av[i][j];
+	for (i = 0; i < ac; i++)
+	{
+	for (j = 0; av[i][j]; j++)
+	result[index++] = av[i][j];
 
-        result[index++] = '\n';
-    }
+	result[index++] = '\n';
+	}
 
-    result[index] = '\0';
+	result[index] = '\0';
 
-    return (result);
+	return (result);
 }
