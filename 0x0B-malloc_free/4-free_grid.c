646d6caecd6a451f5a15dd6de88a6cad989feb45
thevorgx <kh.elallali@gmail.com>
2023-08-08 17:50:45 -0700

    update 3

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 
-void free_grid(int **grid, int height) {
-    if (grid == NULL) {
+void free_grid(int **grid, int height) 
+{
+    if (grid == NULL) 
         return;
-    }
 
-    for (int i = 0; i < height; i++) {
+    for (int i = 0; i < height; i++) 
+    {
         free(grid[i]);
     }
 
