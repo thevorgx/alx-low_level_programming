d5025bbcbbb27aec47c927534f505f5f42380491
thevorgx <kh.elallali@gmail.com>
2023-08-08 18:24:29 -0700

    update 2

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,26 +11,29 @@
  */
 char *argstostr(int ac, char **av)
 {
-    if (ac == 0 || av == NULL)
-        return NULL;
+	int total_length = 0;
+	int i;
+	int index = 0;
+	int j;
 
-    int total_length = 0;
-    for (int i = 0; i < ac; i++)
+    if (ac == 0 || av == NULL)
+        return (NULL);
+
+    for (i = 0; i < ac; i++)
         total_length += strlen(av[i]) + 1;
 
     char *result = (char *)malloc(total_length + 1);
     if (result == NULL)
-        return NULL;
+        return (NULL);
 
-    int index = 0;
-    for (int i = 0; i < ac; i++)
+    for (i = 0; i < ac; i++)
     {
-        for (int j = 0; av[i][j]; j++)
+        for (j = 0; av[i][j]; j++)
             result[index++] = av[i][j];
-        
+
         result[index++] = '\n';
     }
 
     result[index] = '\0';
-    return result;
+    return (result);
 }
