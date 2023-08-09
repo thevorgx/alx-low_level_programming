6c5cca95422c325b22c6fdea7204b40ae446a5f6
thevorgx <kh.elallali@gmail.com>
2023-08-08 19:59:27 -0700

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,27 +1,77 @@
 #include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * countWords - Count the number of words in a string.
- * @str: The input string.
+ * countw - Count the number of words in a string.
+ * @s: The input string.
  *
  * Return: The number of words in the string.
  */
+int countw(char *s)
+{
+	int f, i, v;
+
+	f = 0;
+	v = 0;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+	if (s[i] == ' ')
+	f = 0;
+	else if (f == 0)
+	{
+	f = 1;
+	v++;
+	}
+
+	i++;
+	}
+
+	return (v);
+}
 char **strtow(char *str)
 {
-    int j = 0;
-    int i = 0;
-    int v = 0;
+	char **x, *tmp;
+	int i = 0, v = 0, l = 0, chars = 0, j = 0, de = 0, fin = 0;
 
-    while (str[j] != '\0') {
-        if (str[j] == ' ') {
-            v = 0;
-        } else if (v == 0) {
-            v = 1;
-            i++;
-        }
-        j++;
-    }
+	while (str[l] != '\0')
+	l++;
+	chars = countw(str);
+	if (chars == 0)
+	return (NULL);
 
-    return (i);
+	x = (char **) malloc(sizeof(char *) * (chars + 1));
+	if (x == NULL)
+	return (NULL);
+
+	while (i <= l)
+	{
+	if (str[i] == ' ' || str[i] == '\0')
+	{
+	if (j)
+	{
+	fin = i;
+	tmp = (char *) malloc(sizeof(char) * (j + 1));
+	if (tmp == NULL)
+	return (NULL);
+	while (de < fin)
+	*tmp++ = str[de++];
+	*tmp = '\0';
+	x[v] = tmp - j;
+	v++;
+	j = 0;
+	}
+	}
+	else if (j++ == 0)
+	de = i;
+
+	i++;
+	}
+
+	x[v] = NULL;
+
+	return (x);
 }
+
