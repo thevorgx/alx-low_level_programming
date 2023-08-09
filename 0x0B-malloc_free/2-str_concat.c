e9c886d5ccf2dd16f7dd2e8d32f1c47bd9618b6c
thevorgx <kh.elallali@gmail.com>
2023-08-08 17:16:50 -0700

    task 2 update 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,32 +11,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *conca;
-    unsigned int len1 = 0, len2 = 0, i, j;
+	char *conca;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
+	while (s1[len1] != '\0')
+	len1++;
 
-    while (s2[len2] != '\0')
-        len2++;
+	while (s2[len2] != '\0')
+	len2++;
 
-    conca = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	conca = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
-    if (conca == NULL)
-        return (NULL);
+	if (conca == NULL)
+	return (NULL);
 
-    for (i = 0; i < len1; i++)
-        conca[i] = s1[i];
+	for (i = 0; i < len1; i++)
+	conca[i] = s1[i];
 
-    for (j = 0; j < len2; j++)
-        conca[i + j] = s2[j];
+	for (j = 0; j < len2; j++)
+	conca[i + j] = s2[j];
 
-    conca[i + j] = '\0';
+	conca[i + j] = '\0';
 
-    return (conca);
+	return (conca);
 }
