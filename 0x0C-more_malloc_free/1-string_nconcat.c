d098c25c6b014ddd971413ffa7ffe38bbfe751b2
thevorgx <kh.elallali@gmail.com>
2023-08-10 00:40:59 -0700

    task 2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenates two strings up to n bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes from s2 to concatenate.
+ *
+ * Return: A pointer to the concatenated string, or NULL on failure.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *conca;
+
+	if (s1 == NULL)
+        s1 = "";
+    	if (s2 == NULL)
+        s2 = "";
+
+    unsigned int len1 = strlen(s1);
+    unsigned int len2 = strlen(s2);
+
+    if (n >= len2)
+        n = len2;
+
+    conca = (char *)malloc(len1 + n + 1);
+
+    if (conca == NULL)
+        return (NULL);
+
+    strcpy(conca, s1);
+    strncat(conca, s2, n);
+
+    return (conca);
+}
