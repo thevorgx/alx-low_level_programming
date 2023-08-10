9d43b67338de202fadf8b416a919eb10da58b49e
thevorgx <kh.elallali@gmail.com>
2023-08-10 11:31:14 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -15,23 +16,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *conca;
 
 	if (s1 == NULL)
-        s1 = "";
-    	if (s2 == NULL)
-        s2 = "";
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
 
-    unsigned int len1 = strlen(s1);
-    unsigned int len2 = strlen(s2);
+	unsigned int len1 = strlen(s1);
+	unsigned int len2 = strlen(s2);
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+	n = len2;
 
-    conca = (char *)malloc(len1 + n + 1);
+	conca = (char *)malloc(len1 + n + 1);
 
-    if (conca == NULL)
-        return (NULL);
+	if (conca == NULL)
+	return (NULL);
 
-    strcpy(conca, s1);
-    strncat(conca, s2, n);
+	strcpy(conca, s1);
+	strncat(conca, s2, n);
 
-    return (conca);
+	return (conca);
 }
