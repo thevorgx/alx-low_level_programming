258ba5c56b98db864ffe157b9b8e1af8ca8e1747
thevorgx <kh.elallali@gmail.com>
2023-08-10 12:44:06 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,38 +1,47 @@
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
-
 /**
- * *string_nconcat - concatenates 2 strings.
+ * string_nconcat - Concatenates two strings up to n bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes from s2 to concatenate.
  *
- * @s1: first string.
- * @s2: second string.
- * @n: string length.
- * Return: pointer to concatenated string and NULL on failure.
+ * Return: A pointer to the concatenated string, or NULL on failure.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *conca;
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int i, j;
+
+	while (s1[len1] != '\0')
+	len1++;
+
+	while (s2[len2] != '\0')
+	len2++;
 
 	if (s1 == NULL)
-		s1 = "";
+	s1 = "";
 
 	if (s2 == NULL)
-		s2 = "";
+	s2 = "";
 
 	if (n >= len2)
-		n = len2;
+	n = len2;
 
 	conca = (char *)malloc(len1 + n + 1);
 
 	if (conca == NULL)
-		return (NULL);
+	return (NULL);
 
-	strcpy(conca, s1);
-	strncat(conca, s2, n);
+	for (i = 0; i < len1; i++)
+	conca[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+	conca[i + j] = s2[j];
+
+	conca[i + j] = '\0';
 
 	return (conca);
 }
