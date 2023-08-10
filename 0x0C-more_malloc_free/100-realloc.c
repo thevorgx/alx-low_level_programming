d359586086502e705da49058b2b0dd1ab4421983
thevorgx <kh.elallali@gmail.com>
2023-08-10 16:53:50 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,40 +12,40 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    int *new_ptr;
-    unsigned int i;
+	int *new_ptr;
+	unsigned int i;
 
-    /* If ptr is NULL, equivalent to malloc(new_size) */
-    if (ptr == NULL)
-        return (malloc(new_size));
+	/* If ptr is NULL, equivalent to malloc(new_size) */
+	if (ptr == NULL)
+	return (malloc(new_size));
 
-    /* If new_size is 0, equivalent to free(ptr) */
-    if (new_size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	/* If new_size is 0, equivalent to free(ptr) */
+	if (new_size == 0)
+	{
+	free(ptr);
+	return (NULL);
+	}
 
-    /* If new_size equals old_size, no reallocation needed */
-    if (new_size == old_size)
-        return (ptr);
+	/* If new_size equals old_size, no reallocation needed */
+	if (new_size == old_size)
+	return (ptr);
 
-    /* Allocate new memory block */
-    new_ptr = malloc(new_size);
+	/* Allocate new memory block */
+	new_ptr = malloc(new_size);
 
-    /* If malloc fails, return NULL */
-    if (new_ptr == NULL)
-        return (NULL);
+	/* If malloc fails, return NULL */
+	if (new_ptr == NULL)
+	return (NULL);
 
-    /* Copy content from old ptr to new_ptr */
-    if (new_size > old_size)
-        new_size = old_size;
+	/* Copy content from old ptr to new_ptr */
+	if (new_size > old_size)
+	new_size = old_size;
 
-    for (i = 0; i < new_size; i++)
-        *((char *)new_ptr + i) = *((char *)ptr + i);
+	for (i = 0; i < new_size; i++)
+	*((char *)new_ptr + i) = *((char *)ptr + i);
 
-    /* Free the old memory block */
-    free(ptr);
+	/* Free the old memory block */
+	free(ptr);
 
-    return (new_ptr);
+	return (new_ptr);
 }
