5beca30d89b5135b66a6e887aa6e64f9cf897dbe
thevorgx <kh.elallali@gmail.com>
2023-08-10 11:27:31 -0700

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,15 +7,15 @@
  * @b: Number of bytes to allocate.
  *
  * Return: Pointer to allocated memory.
- *         Exits with status 98 if allocation fails.
+ *	Exits with status 98 if allocation fails.
  */
 
 void *malloc_checked(unsigned int b)
 {
-        int *v;
+	int *v;
 
-        v = malloc(b);
-        if (v == NULL)
-                exit(98);
-        return (v);
+	v = malloc(b);
+	if (v == NULL)
+	exit(98);
+	return (v);
 }
