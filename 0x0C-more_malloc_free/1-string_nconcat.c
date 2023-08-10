5dc27828cbcd1b29b44ad5420a2d9639554a8d7c
thevorgx <kh.elallali@gmail.com>
2023-08-10 11:59:31 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,18 +18,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len2 = strlen(s2);
 
 	if (s1 == NULL)
-	s1 = "";
+		s1 = "";
 
 	if (s2 == NULL)
-	s2 = "";
+		s2 = "";
 
 	if (n >= len2)
-	n = len2;
+		n = len2;
 
 	conca = (char *)malloc(len1 + n + 1);
 
 	if (conca == NULL)
-	return (NULL);
+		return (NULL);
 
 	strcpy(conca, s1);
 	strncat(conca, s2, n);
