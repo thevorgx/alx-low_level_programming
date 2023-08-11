bf35931639b9939c6d371215354300d607f0f47d
thevorgx <kh.elallali@gmail.com>
2023-08-10 18:12:54 -0700

    task 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings representing the arguments
+ *
+ * Return: 0 on success, 98 on incorrect arguments, 98 on non-digit input
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
+        return (98);
+    }
+
+    for (int i = 1; i < 3; i++)
+    {
+        char *num = argv[i];
+        while (*num)
+        {
+            if (*num < '0' || *num > '9')
+            {
+                _putchar('E');
+                _putchar('r');
+                _putchar('r');
+                _putchar('o');
+                _putchar('r');
+                _putchar('\n');
+                return (98);
+            }
+            num++;
+        }
+    }
+
+    long long result = atoll(argv[1]) * atoll(argv[2]);
+    char buffer[20];
+    int len = 0;
+
+    if (result == 0)
+        _putchar('0');
+    else
+    {
+        while (result)
+        {
+            buffer[len++] = result % 10 + '0';
+            result /= 10;
+        }
+        for (int i = len - 1; i >= 0; i--)
+            _putchar(buffer[i]);
+    }
+
+    _putchar('\n');
+
+    return (0);
+}
