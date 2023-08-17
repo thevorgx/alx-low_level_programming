8633a7a14dbd0e2bf0b10c710a4013eccaa5ec14
thevorgx <kh.elallali@gmail.com>
2023-08-16 19:06:00 -0700

    update

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns the sum of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference between two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference between a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the division result of two integers.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The division result of a and b.
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of division between two integers.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The remainder of the division between a and b.
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+	return (a % b);
+}
