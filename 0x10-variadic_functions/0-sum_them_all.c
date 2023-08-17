2dc0389ed9fdad781f428f32a40f1282118a8241
thevorgx <kh.elallali@gmail.com>
2023-08-17 15:58:45 -0700

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,29 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
+
 /**
- * sum_them_all - Calculate the sum of a variable number of unsigned integers.
- * @n: The number of unsigned integers to sum.
- * @...: Variable number of unsigned integers to be summed.
- * * Return: The sum of the provided unsigned integers, or 0 if @n is 0.
- */
+*sum_them_all - Calculate the sum of a variable number of unsigned integers.
+*@n: The number of unsigned integers to sum.
+*@...: Variable number of unsigned integers to be summed.
+*Return: The sum of the provided unsigned integers, or 0 if @n is 0.
+*/
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
 	unsigned int i;
-	va_list arguments;
+	unsigned int res = 0;
 
-	va_start(arguments, n);
+	va_list args;
+
+	va_start(args, n);
 
 	if (n == 0)
 	return (0);
+
 	for (i = 0; i < n; i++)
 	{
-	sum += va_arg(arguments, unsigned int);
+	res += va_arg(args, unsigned int);
+	va_end(args);
 	}
-	return (sum);
+
+	return (res);
 }
