156016d5117e5ae04ef882eb7869f9b6a04174fd
thevorgx <kh.elallali@gmail.com>
2023-08-24 19:29:45 -0700

    before the main func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+/**
+ * star - Function with constructor attribute to print a message on startup
+ * auto before main.
+ *
+ */
+void star(void) __attribute__ ((constructor));
+void star(void) __attribute__ ((constructor))
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
