758fcd14f10ecadd7252369d3409bf27d2a12049
thevorgx <kh.elallali@gmail.com>
2023-08-24 19:33:35 -0700

    iq -7000 when you loose that much of a sleep

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  *
  */
 void star(void) __attribute__ ((constructor));
-void star(void) __attribute__ ((constructor))
+void star(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
