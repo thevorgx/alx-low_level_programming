49aeade4a0a8a249a409d3c95f813d138aad7ae9
thevorgx <kh.elallali@gmail.com>
2023-09-05 01:58:51 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,24 +23,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
 
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
-	{
-		close(fd);
 		return (0);
-	}
 
 	bytes_written = write(1, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(fd);
 		return (0);
-	}
 
 	free(buffer);
 	close(fd);
