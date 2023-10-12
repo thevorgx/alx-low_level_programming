69217efd7dfeb71f37247f90ed7c9051a6c995aa
thevorgx <kh.elallali@gmail.com>
2023-10-12 23:01:04 +0100

    add node task 2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,7 +4,7 @@
  * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: A pointer to the head of the doubly-linked list.
  *
- * Return: The number of nodes in the list.
+ * Return: The number of elements in the list.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
