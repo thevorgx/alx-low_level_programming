198a035b8a2f79e42dd221c99370fede21ddadc6
thevorgx <kh.elallali@gmail.com>
2023-10-13 01:39:12 +0100

    betty documentation

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * sum_dlistint - Sums the elements of a doubly linked list.
+ * @head: A pointer to the head of the list.
+ *
+ * Return: The sum of elements , or 0 if the list empty.
+ */
 
 int sum_dlistint(dlistint_t *head)
 {
