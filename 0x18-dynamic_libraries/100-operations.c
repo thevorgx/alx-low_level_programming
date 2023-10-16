3f275fe11d379b4beb89968af40c2033838c4c63
thevorgx <kh.elallali@gmail.com>
2023-10-16 13:25:01 +0100

    task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int res = a + b;
+    return (res);
+}
+
+int sub(int a, int b)
+{
+    int res = a - b;
+    return (res);
+}
+
+int mul(int a, int b)
+{
+    int res = a * b;
+    return (res);
+}
+
+int dev(int a, int b)
+{
+    int res = a / b;
+    return (res);
+}
+
+int mod(int a, int b)
+{
+    int res = a % b;
+    return (res);
+}
